89b542d8387fc3871086b525fa3ce03bbd5328a1
xavier <xavier@fed29onDellXPS>
2019-06-26 13:58:05 +0200

    better checks

--- pkg/backprop/util.go
+++ pkg/backprop/util.go
@@ -20,14 +20,18 @@ func SplitTrainTest(x *mat.Dense, ratio float64) (train, test *mat.Dense) {
 }
 
 // Shuffle input matrix rows IN PLACE
-// Shufle happens based on the numer of rows of the first matrix.
+// All matrix should have same number of rows
+// Shufle happens based on the number of rows of the first matrix.
 func Shuffle(xx ...*mat.Dense) {
 
 	r, _ := xx[0].Dims()
 
 	rand.Shuffle(r, func(i, j int) {
 		for _, x := range xx {
-			_, c := x.Dims()
+			rr, c := x.Dims()
+			if rr != r {
+				panic("All input matrices should have same number of rows")
+			}
 			for cc := 0; cc < c; cc++ {
 				a, b := x.At(i, cc), x.At(j, cc)
 				x.Set(i, cc, b)
