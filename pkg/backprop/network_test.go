10d38ffc84fd42ad9a59e255d1a0ac13d11722b3
xavier <xavier@fed29onDellXPS>
2019-07-04 14:59:34 +0200

    sigmoid

--- pkg/backprop/network_test.go
+++ pkg/backprop/network_test.go
@@ -127,15 +127,15 @@ func TestTrainIris(t *testing.T) {
 
 	net := NewMLNetwork(
 		NewFCLayer(4, 35),
-		NewFCLayer(35, 3)).
+		NewFCLayer(35, 3).SetActivation(ActivationSigmoid)).
 		SetCost(CostMSE).
 		InitWB(InitializationRandom)
 
 	var c, cc float64
-	for i := 0; i < 30; i++ {
-		c = net.Train(xtrain, ytrain, 0.001, 0.01, 500)
+	for i := 0; i < 40; i++ {
+		c = net.Train(xtrain, ytrain, 0.05, 0.01, 100)
 		cc = net.Evaluate(xvalidate, yvalidate)
-		fmt.Printf("%dx%d\t Cost training : %e\t validation: %e\n", i, 500, c, cc)
+		fmt.Printf("%dx%d\t Cost training : %e\t validation: %e\n", i, 100, c, cc)
 	}
 
 	if display {
